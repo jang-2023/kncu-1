d3ae4f636fb785a56d653a6c90a8625308cd9aba
Jang-2023 <jej0360@hanmail.net>
2023-12-12 16:13:31 +0900

    웹사이트 프로젝트

---
+++ _men.php
@@ -0,0 +1,30 @@
+<?php
+  $servername ="localhost"; // 서버명
+ $username = "root";    // 사용자명
+ $password = ""; // 비밀번호
+ $dbname = "database";  // DB명
+
+ // MySQL 연결하기
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+$sql = "CREATE TABLE  _mem (
+    num int not null auto_increment,
+    id char(20) not null,
+    pass char(20) not null,
+    name char(20) not null,
+    email char(80),
+    regist_day char(20),
+    level int,
+    point int,
+    primary key(num))";
+
+
+ $result = mysqli_query($conn, $sql);
+ if ($result) 
+
+ echo "회원 db 테이블에 생성 완료!";
+ else 
+ echo "회원 db 테이블에 생성 오류 : " . mysqli_error($conn);
+mysqli_close($conn);
+	
+?>
---
+++ member/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$type = $_GET["type"];
+
+include "../header.php";
+include $type.".php";
+
+?>
